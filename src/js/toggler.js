9d31187c4cee6ac9b668a44eab3e10278936529a
webistomin <webistomin@gmail.com>
2018-01-29 19:12:54 +0300

    сделал menu

--- src/js/toggler.js
+++ src/js/toggler.js
@@ -1,15 +1,15 @@
-var navMain = document.querySelector('.main-nav');
+var navMain = document.querySelector('.main-nav__list');
 var navToggle = document.querySelector('.main-nav__toggler');
-navMain.classList.remove('main-nav--nojs');
+
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav__list--closed')) {
+    navMain.classList.remove('main-nav__list--closed');
+    navMain.classList.add('main-nav__list--opened');
     navToggle.classList.add('main-nav__toggler--opened')
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav__list--closed');
+    navMain.classList.remove('main-nav__list--opened');
     navToggle.classList.remove('main-nav__toggler--opened');
   }
 });
