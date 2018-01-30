8ea7c8de3a92555738ed9b07c476197c19a836f1
webistomin <webistomin@gmail.com>
2018-01-30 12:43:38 +0300

    сделал dropdown

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -2,14 +2,14 @@ var list = document.querySelector('.main-nav__dropdown');
 var link = document.querySelector('.main-nav__link--dropdown');
 
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav__dropdown--closed')) {
-    navMain.classList.remove('main-nav__dropdown--closed');
-    navMain.classList.add('main-nav__dropdown--opened');
-    navToggle.classList.add('main-nav__link--opened')
+link.addEventListener('click', function() {
+  if (list.classList.contains('main-nav__dropdown--closed')) {
+    list.classList.remove('main-nav__dropdown--closed');
+    list.classList.add('main-nav__dropdown--opened');
+    link.classList.add('main-nav__link--opened')
   } else {
-    navMain.classList.add('main-nav__dropdown--closed');
-    navMain.classList.remove('main-nav__dropdown--opened');
-    navToggle.classList.remove('main-nav__link--opened');
+    list.classList.add('main-nav__dropdown--closed');
+    list.classList.remove('main-nav__dropdown--opened');
+    link.classList.remove('main-nav__link--opened');
   }
 });
