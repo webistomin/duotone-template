c82e15fa5e6c167061fb11f887f2b6d54bd1695c
webistomin <webistomin@gmail.com>
2018-02-01 10:55:20 +0300

    сделал блок quotes

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -6,7 +6,7 @@ link.addEventListener('click', function() {
   if (list.classList.contains('main-nav__dropdown--closed')) {
     list.classList.remove('main-nav__dropdown--closed');
     list.classList.add('main-nav__dropdown--opened');
-    link.classList.add('main-nav__link--opened')
+    link.classList.add('main-nav__link--opened');
   } else {
     list.classList.add('main-nav__dropdown--closed');
     list.classList.remove('main-nav__dropdown--opened');
